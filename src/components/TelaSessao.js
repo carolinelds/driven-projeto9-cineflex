ffc18c81644c7198642a97ce1632bc7db692769d
Caroline LS <carolinelds@gmail.com>
2022-03-28 15:43:56 -0300

    Exibe dias e horarios das sessoes dos filmes, obtendo os dados via API

--- src/components/TelaSessao.js
+++ src/components/TelaSessao.js
@@ -1,5 +1,56 @@
-export default function TelaSessao(){
-    return (
-        <h1>testando a tela sessao</h1>
-    );
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
+export default function TelaSessao() {
+
+    const { idFilme } = useParams();
+    const [filme, setFilme] = useState({days: []});
+
+    useEffect(() => {
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
+        promise.then(response => {
+            const { data } = response;
+            //console.log(data);
+            setFilme(data);
+        })
+    }, []);
+
+    const sessoes = filme.days;
+
+    return sessoes.length > 0 ? (
+        <div class="TelaSessao">
+            <p>Selecione o horário</p>
+            <div class="container-sessoes">
+                {
+                    sessoes.map(day =>{
+                        const { weekday, date } = day;
+                        
+                        return (
+                            <div class="dia-horarios" key={day.id}>
+                                <p>{weekday} - {date}</p>
+
+                                <div class="container-horarios">
+                                    {
+                                        day.showtimes.map(horario =>{
+                                            const {name} = horario;
+
+                                            return (
+                                                <div class="horario" key={horario.id}>
+                                                    <p>{name}</p>
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            
+                        );
+                        
+                    })
+                }
+            </div>
+        </div>
+    ) : <p>Carregando...</p>
 }
